dd3d65c03ef816bcf863ea17da2f5c1869db7366
Rushing <liming20110711@163.com>
2019-09-30 01:41:36 +0800

    update:20190930-02

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,21 +1,25 @@
 <template>
-    <el-row :gutter="15" class="m-header">
-        <el-col :span="6">
-            <div class="grid-content bg-purple"></div>
+    <el-row :gutter="0" class="m-header">
+        <el-col :span="4">
+            <geo />
         </el-col>
         <el-col :span="6">
-            <div class="grid-content bg-purple"></div>
+            <user />
         </el-col>
-        <el-col :span="6">
-            <div class="grid-content bg-purple"></div>
+        <el-col :span="14">
+            <navbar />
         </el-col>
     </el-row>
 </template>
 <script>
 import Geo from './geo';
+import User from './user';
+import Navbar from './nav';
 export default {
     components: {
-        Geo
+        Geo,
+        User,
+        Navbar
     }
 };
 </script>
